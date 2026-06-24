33a9ee3303987c2e258135f97566539df389f10e
srijan_ananda <srijanjas78@gmail.com>
2026-06-25 02:06:49 +0530

    feat: add outputs for VPC ID, EC2 IP, and S3 bucket name

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "ec2_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.web.public_dns
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.main.bucket
+}
